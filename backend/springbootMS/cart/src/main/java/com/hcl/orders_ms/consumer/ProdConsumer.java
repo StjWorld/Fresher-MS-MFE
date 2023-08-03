80282ac0887ec3e551b488676f4ba61e38268d65
Wyatt <43044773+WyattLiang@users.noreply.github.com>
2023-08-03 13:11:37 -0400

    have product consumer

--- backend/springbootMS/cart/src/main/java/com/hcl/orders_ms/consumer/ProdConsumer.java
+++ backend/springbootMS/cart/src/main/java/com/hcl/orders_ms/consumer/ProdConsumer.java
@@ -1,17 +1,18 @@
 package com.hcl.orders_ms.consumer;
 
+import com.hcl.orders_ms.config.ProdAck;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class ProdConsumer {
     private static final Logger LOGGER
             = LoggerFactory.getLogger(ProdConsumer.class);
 
     @RabbitListener(queues={"${rabbitmq.queue.name.prod}"})
-    public void consumerJsonMessage(Boolean yesOrno){
+    public void consumerJsonMessage(ProdAck yesOrno){
         LOGGER.info(String.format("Received JSON message -> %s",yesOrno));
     }
 }
